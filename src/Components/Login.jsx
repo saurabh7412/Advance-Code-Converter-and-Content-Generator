f050d6e7701fa9d9fe3e82408fef8531131e21b8
saurabh7412 <saurabh7412@gmail.com>
2024-07-23 01:02:42 +0530

    another

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
   return (
     <div style={{}}>
       <a
-        href={`${process.env.REACT_APP_OAUTH_BASE_URL}?client_id=${process.env.REACT_APP_CLIENT_ID}&scope=repo&redirect_uri=${encodeURIComponent("https://advance-code-converter-and-content-generator.vercel.app/code-convertor")}`}
+        href={`${process.env.REACT_APP_OAUTH_BASE_URL}?client_id=${process.env.REACT_APP_CLIENT_ID}&scope=repo&redirect_uri=https://advance-code-converter-and-content-generator.vercel.app/code-converter`}
         // href={`${process.env.REACT_APP_OAUTH_BASE_URL}?client_id=${process.env.REACT_APP_CLIENT_ID}&scope=repo&redirect_uri=http://localhost:3000/`}
         style={{
           textDecoration: "none",
